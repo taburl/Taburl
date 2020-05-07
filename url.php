c285232e9822a31c9bed25a75d420091c2f6a722
Taburl <64861099+taburl@users.noreply.github.com>
2020-05-07 10:26:21 +0800

    Update url.php

--- url.php
+++ url.php
@@ -4,7 +4,7 @@ $userInfo = $_POST;
 $data =array('respData'=>$userInfo);
 $json = json_encode($data);
 //打开json文件,如果没有json文件则自动创建json文件
-$file = fopen('form.json','w+');
+$file = fopen('https://taburl.github.io/Taburl/form.json','w+');
 //把json字符串$json写入json文件form.json
 fwrite($file,$json);
 //关闭文件
